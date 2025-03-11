9f38c46877c90bbb60eb1a2810eb5c6f980c3ef1
gnl2024 <gnlauture@gmail.com>
2025-03-11 09:30:22 -0400

    added premises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const axios = require('axios');
+
+public_users.get('/', async (req, res) => {
+    try {
+        // Simulate fetching books asynchronously
+        const response = await axios.get('http://localhost:5001/books');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching books", error: error.message });
+    }
+});
+
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -16,25 +28,60 @@ public_users.get('/', (req, res) => {
 });
 
 
-// Get book details based on ISBN
+// Get book details based on ISBN using premises
+
 public_users.get('/isbn/:isbn', (req, res) => {
-    const book = books[req.params.isbn];
-    book ? res.json(book) : res.status(404).json({ message: "Book not found" });
+    const isbn = req.params.isbn;
+
+    new Promise((resolve, reject) => {
+        const book = books[isbn];
+        if (book) {
+            resolve(book);
+        } else {
+            reject("Book not found");
+        }
+    })
+    .then(book => res.json(book))
+    .catch(error => res.status(404).json({ message: error }));
 });
+
 
   
-// Get book details based on author
+// Get book details based on author using premises
 public_users.get('/author/:author', (req, res) => {
-    const result = Object.values(books).filter(book => book.author === req.params.author);
-    result.length ? res.json(result) : res.status(404).json({ message: "No books found by this author" });
+    const author = req.params.author;
+
+    new Promise((resolve, reject) => {
+        const booksByAuthor = Object.values(books).filter(book => book.author === author);
+        if (booksByAuthor.length) {
+            resolve(booksByAuthor);
+        } else {
+            reject("No books found by this author");
+        }
+    })
+    .then(books => res.json(books))
+    .catch(error => res.status(404).json({ message: error }));
 });
 
 
-// Get all books based on title
+
+
+// Get all books based on title using premises
 public_users.get('/title/:title', (req, res) => {
-    const result = Object.values(books).filter(book => book.title === req.params.title);
-    result.length ? res.json(result) : res.status(404).json({ message: "No books found with this title" });
+    const title = req.params.title;
+
+    new Promise((resolve, reject) => {
+        const booksByTitle = Object.values(books).filter(book => book.title === title);
+        if (booksByTitle.length) {
+            resolve(booksByTitle);
+        } else {
+            reject("No books found with this title");
+        }
+    })
+    .then(books => res.json(books))
+    .catch(error => res.status(404).json({ message: error }));
 });
+
 
 
 //  Get book review
